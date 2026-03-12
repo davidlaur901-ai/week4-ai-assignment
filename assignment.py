bc4e4278bacae075fa1d34a2df0d6ed02b9e9c37
davidlaur901-ai <davidlaur901@gmail.com>
2026-03-12 21:27:36 +0300

    Add initial assignment.py with questions and prints

---
+++ assignment.py
@@ -0,0 +1,18 @@
+
+# Question 4
+print("Hello AI Students!")
+
+ # Question 5
+name = "david laur"
+age = 30
+city = "Nairobi"
+print("My name is", name, "I am", age, "years old and I live in", city)
+
+# Question 6
+Programminglanguages=["Python", "Java", "C++", "JavaScript"]
+print(Programminglanguages[0])
+
+# Question 7
+county = {"USA": 331, "India": 1380, "China": 1439}
+print(county["USA"])
+
